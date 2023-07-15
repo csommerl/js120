304c5148260bbc631d63d6c44679df9b36cfd336
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2023-07-15 13:31:04 -0500

    ...

--- lesson3/dog.js
+++ lesson3/dog.js
@@ -1,28 +1,22 @@
 function Dog(name, breed, weight) {
-  Object.setPrototypeOf(this, Dog.myPrototype);
   this.name = name;
   this.breed = breed;
   this.weight = weight;
 }
 
-Dog.myPrototype = {
-  bark: function() {
-    console.log(this.weight > 20 ? 'Woof!' : 'Yip!');
-  },
+console.log(Dog.prototype); // {}
+console.log(Dog.prototype.constructor === Dog); // true
+
+Dog.prototype.bark = function() {
+  console.log(this.weight > 20 ? 'Woof!' : 'Yip!');
 };
+
+console.log(Dog.prototype); // { bark: [Function (anonymous)] }
 
 let maxi = new Dog('Maxi', 'German Shepherd', 32);
 let dexter = new Dog('Dexter', 'Rottweiler', 50);
 let biggie = new Dog('Biggie', 'Whippet', 9);
 
-maxi.bark(); // 'Woof!'
+// maxi.bark(); // 'Woof!'
 
-console.log(
-  maxi.hasOwnProperty('bark'),
-  dexter.hasOwnProperty('bark'),
-  biggie.hasOwnProperty('bark'),
-  maxi.bark === dexter.bark,
-  maxi.bark === biggie.bark,
-  dexter.bark === biggie.bark,
-  Object.getPrototypeOf(maxi).bark === Dog.myPrototype.bark,
-);
+console.log(Dog.prototype.constructor === Dog); // true
