0a600c0f41df49ea4b7bbcc8daa44910fb0c46cb
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2023-07-26 13:56:13 -0500

    ...

---
+++ patterns/class.js
@@ -0,0 +1,96 @@
+class Person {
+  constructor(firstName, lastName, age, gender) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  communicate() {
+    console.log('Communicating');
+  }
+
+  eat() {
+    console.log('Eating');
+  }
+
+  sleep() {
+    console.log('Sleeping');
+  }
+}
+
+class Doctor extends Person {
+  constructor(firstName, lastName, age, gender, specialization) {
+    super(firstName, lastName, age, gender);
+    this.specialization = specialization;
+  }
+
+  diagnose() {
+    console.log('Diagnosing');
+  }
+}
+
+class Professor extends Person {
+  constructor(firstName, lastName, age, gender, subject) {
+    super(firstName, lastName, age, gender);
+    this.subject = subject;
+  }
+
+  teach() {
+    console.log('Teaching');
+  }
+}
+
+class Student extends Person {
+  constructor(firstName, lastName, age, gender, degree) {
+    super(firstName, lastName, age, gender);
+    this.degree = degree;
+  }
+
+  study() {
+    console.log('Studying');
+  }
+}
+
+class GraduateStudent extends Student {
+  constructor(firstName, lastName, age, gender, degree, graduateDegree) {
+    super(firstName, lastName, age, gender, degree);
+    this.graduateDegree = graduateDegree;
+  }
+
+  research() {
+    console.log('Researching');
+  }
+}
+
+let person = new Person('foo', 'bar', 21, 'gender');
+console.log(person instanceof Person);     // logs true
+person.eat();                              // logs 'Eating'
+person.communicate();                      // logs 'Communicating'
+person.sleep();                            // logs 'Sleeping'
+console.log(person.fullName());            // logs 'foo bar'
+
+let doctor = new Doctor('foo', 'bar', 21, 'gender', 'Pediatrics');
+console.log(doctor instanceof Person);     // logs true
+console.log(doctor instanceof Doctor);     // logs true
+doctor.eat();                              // logs 'Eating'
+doctor.communicate();                      // logs 'Communicating'
+doctor.sleep();                            // logs 'Sleeping'
+console.log(doctor.fullName());            // logs 'foo bar'
+doctor.diagnose();                         // logs 'Diagnosing'
+
+let graduateStudent = new GraduateStudent('foo', 'bar', 21, 'gender', 'BS Industrial Engineering', 'MS Industrial Engineering');
+// logs true for next three statements
+console.log(graduateStudent instanceof Person);
+console.log(graduateStudent instanceof Student);
+console.log(graduateStudent instanceof GraduateStudent);
+graduateStudent.eat();                     // logs 'Eating'
+graduateStudent.communicate();             // logs 'Communicating'
+graduateStudent.sleep();                   // logs 'Sleeping'
+console.log(graduateStudent.fullName());   // logs 'foo bar'
+graduateStudent.study();                   // logs 'Studying'
+graduateStudent.research();                // logs 'Researching'
