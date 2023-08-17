3e36b6a87fac0f41561dfc45075c8e495adac098
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2023-08-17 12:47:23 -0500

    simple circular queue

---
+++ exercises/object_creation_patterns/circular.queue.js
@@ -0,0 +1,64 @@
+// https://launchschool.com/exercises/1becc424
+
+class CircularQueue {
+  constructor(size) { // STUB
+    this.queue = [];
+    this.size = size;
+  }
+
+  enqueue(obj) { // STUB
+    if (this.queue.length < this.size) {
+      this.queue.push(obj);
+    } else {
+      this.queue.shift();
+      this.queue.push(obj);
+    }
+  }
+
+  dequeue() { // STUB
+    if (this.queue.length === 0) {
+      return null;
+    } else {
+      return this.queue.shift();
+    }
+  }
+}
+
+let queue = new CircularQueue(3);
+console.log(queue.dequeue() === null);
+
+queue.enqueue(1);
+queue.enqueue(2);
+console.log(queue.dequeue() === 1);
+
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue() === 2);
+
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(7);
+console.log(queue.dequeue() === 5);
+console.log(queue.dequeue() === 6);
+console.log(queue.dequeue() === 7);
+console.log(queue.dequeue() === null);
+
+let anotherQueue = new CircularQueue(4);
+console.log(anotherQueue.dequeue() === null);
+
+anotherQueue.enqueue(1)
+anotherQueue.enqueue(2)
+console.log(anotherQueue.dequeue() === 1);
+
+anotherQueue.enqueue(3)
+anotherQueue.enqueue(4)
+console.log(anotherQueue.dequeue() === 2);
+
+anotherQueue.enqueue(5)
+anotherQueue.enqueue(6)
+anotherQueue.enqueue(7)
+console.log(anotherQueue.dequeue() === 4);
+console.log(anotherQueue.dequeue() === 5);
+console.log(anotherQueue.dequeue() === 6);
+console.log(anotherQueue.dequeue() === 7);
+console.log(anotherQueue.dequeue() === null);
